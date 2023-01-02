ec8077ec44bbaf6d2b60433196bb13596a38aadb
gangaprasadmohite <gemohite@gmail.com>
2023-01-02 17:08:21 +0530

    Added routing

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import ClientListPage from './ClientListPage';
 import Box from '@mui/material/Box';
-import { BrowserRouter as Router, Routes, Route } from 'react-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ClientDetailsPage from './ClientDetailsPage';
 import ClientPortfolioPage from './ClientPortfolioPage';
 import ClientOrderListingPage from './ClientOrderListingPage';
@@ -11,10 +11,18 @@ import PropertyDetails from './PropertyDetails';
 const MainPage = () => (
   <Box>
     {/* <ClientListPage /> */}
-    {/* <ClientDetailsPage /> */}
-    {/* <ClientPortfolioPage /> */}
-    {/* <ClientOrderListingPage /> */}
-    <PropertyDetails />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<ClientListPage />} />
+        <Route path="/client-details" element={<ClientDetailsPage />} />
+        <Route path="/client-portfolio" element={<ClientPortfolioPage />} />
+        <Route
+          path="/client-order-listing"
+          element={<ClientOrderListingPage />}
+        />
+        <Route path="/property-details" element={<PropertyDetails />} />
+      </Routes>
+    </Router>
   </Box>
 );
 
